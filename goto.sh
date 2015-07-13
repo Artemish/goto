b68fe025005324c70630a9bd0f49186a48d85075
Mitch Croal <mitch.cro@gmail.com>
2015-07-13 16:26:28 -0500

    Fixed indentation

--- goto.sh
+++ goto.sh
@@ -25,83 +25,83 @@ __read_gotodb() {
   done < ${__alias_file}
 }
 
-  __write_gotodb() {
-    # Clear the alias file
-    > ${__alias_file}
-    for key in "${!GOTODB[@]}"; do
-      local value="${GOTODB[${key}]}"
-      echo "${key}:${value}" >> ${__alias_file}
-    done
-  }
+__write_gotodb() {
+  # Clear the alias file
+  > ${__alias_file}
+  for key in "${!GOTODB[@]}"; do
+    local value="${GOTODB[${key}]}"
+    echo "${key}:${value}" >> ${__alias_file}
+  done
+}
 
-  goto() {
-    local usage="\
-  Usage: goto alias
-      [-m destination] alias
-      [-x alias] alias
-      -l"
+goto() {
+  local usage="\
+    Usage: goto alias
+  [-m destination] alias
+  [-x alias] alias
+  -l"
 
-    if [[ $# == 0 ]] ; then
+  if [[ $# == 0 ]] ; then
+    echo "$usage"
+    return 1
+  fi
+
+  if [ -z ${GOTODB} ]; then 
+    __read_gotodb
+  fi
+
+  # getopt seemed like overkill
+  # 'm' for making a new alias
+  if [[ $1 == '-m' ]] ; then
+    if [[ $# -ne 3 ]]; then
       echo "$usage"
       return 1
     fi
-    
-    if [ -z ${GOTODB} ]; then 
-      __read_gotodb
+
+    local abspath=$(cd "$(dirname "$2")"; pwd)/"$(basename "$2")"
+
+    if ! [[ -e $abspath ]] ; then
+      echo "No such directory: $abspath"
+      return 2
     fi
 
-    # getopt seemed like overkill
-    # 'm' for making a new alias
-    if [[ $1 == '-m' ]] ; then
-      if [[ $# -ne 3 ]]; then
-        echo "$usage"
-        return 1
-      fi
+    GOTODB["$3"]=${abspath}
 
-      local abspath=$(cd "$(dirname "$2")"; pwd)/"$(basename "$2")"
-
-      if ! [[ -e $abspath ]] ; then
-        echo "No such directory: $abspath"
-        return 2
-      fi
-
-      GOTODB["$3"]=${abspath}
-
-      __write_gotodb
+    __write_gotodb
 
     # 'd' for deleting an alias
-    elif [[ $1 == '-d' ]]; then
-      if [[ $# -ne 2 ]]; then
-        echo "$usage"
-        return 1
-      fi
+  elif [[ $1 == '-d' ]]; then
+    if [[ $# -ne 2 ]]; then
+      echo "$usage"
+      return 1
+    fi
 
-      local result=${GOTODB[$2]}
-      if [[ -z "$result" ]] ; then
-        echo "No alias for '$2'."
-        return -1
-      fi
+    local result=${GOTODB[$2]}
+    if [[ -z "$result" ]] ; then
+      echo "No alias for '$2'."
+      return -1
+    fi
 
-      unset GOTODB["$2"]
-      __write_gotodb
+    unset GOTODB["$2"]
+    __write_gotodb
 
     # 'l' for listing aliases
-    elif [[ $1 == '-l' ]]; then
-      for key in "${!GOTODB[@]}"; do
-        echo "${key} -> ${GOTODB[${key}]}" 
-      done | column -t 
+  elif [[ $1 == '-l' ]]; then
+    for key in "${!GOTODB[@]}"; do
+      echo "${key} -> ${GOTODB[${key}]}" 
+    done | column -t 
 
     # default case - find the entry and CD to it
+  else
+    local result=${GOTODB[$1]}
+    if [[ -e "$result" ]] ; then
+      cd "$result"
+      return 0
     else
-      local result=${GOTODB[$1]}
-      if [[ -e "$result" ]] ; then
-        cd "$result"
-        return 0
-      else
-        echo "No alias for '$1'."
-        return -1
-      fi
+      echo "No alias for '$1'."
+      return -1
     fi
+  fi
 }
 
 __read_gotodb
